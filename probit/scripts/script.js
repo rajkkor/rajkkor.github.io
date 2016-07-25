4ec834a4ecd006a6b9595abec53690b8987ddcd9
rajkkor <vincent.grenfeldt@gmail.com>
2016-07-25 11:50:16 +0200

    adds font-awesome and changes navbar behaviour

--- probit/scripts/script.js
+++ probit/scripts/script.js
@@ -10,3 +10,25 @@ $(document).ready(function () {
         $(this).addClass('active');
     });
 });
+function onScroll() {
+    var nav = $('#navbar');
+    var offset = nav.outerHeight();
+    //noinspection JSValidateTypes
+    if ($(document).scrollTop() > offset) {
+        nav.css({
+            "position": "fixed"
+            , "top": "0px"
+        });
+        $(".nav-box").animate({
+            "padding": "20px 40px"
+        }, 200);
+    } else {
+        nav.css({
+            "position": "relative"
+            , "top": "'" + offset + "'"
+        });
+        $(".nav-box").animate({
+            "padding": "10px 30px"
+        }, 200);
+    }
+}
